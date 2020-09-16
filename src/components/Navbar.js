cea94e7d464dc0b3ead83b395da817c70586aeb2
Salnikov Maksim <salnikov-m@yandex.ru>
2020-09-16 12:22:45 +0400

    Lesson3 is done

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -1,4 +1,5 @@
 import React from "react";
+import {NavLink} from "react-router-dom";
 
 
 export const Navbar = () => {
@@ -9,10 +10,10 @@ export const Navbar = () => {
       </div>
       <ul className="navbar-nav">
         <li className="nav-item">
-          <a href="/" className="nav-link">Главная</a>
+          <NavLink exact to="/" className="nav-link">Главная</NavLink>
         </li>
         <li className="nav-item">
-          <a href="/about" className="nav-link">Информация</a>
+          <NavLink to="/about" className="nav-link">Информация</NavLink>
         </li>
       </ul>
     </nav>
