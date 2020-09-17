cd7102ddae9019f0b4ab131c5848b72f3e5cb376
Salnikov Maksim <salnikov-m@yandex.ru>
2020-09-17 12:01:23 +0400

    Hooks. Lesson 11 is done!

--- src/context/github/GithubState.js
+++ src/context/github/GithubState.js
@@ -7,6 +7,11 @@ import axios from 'axios'
 const CLIENT_ID = process.env.REACT_APP_CLIENT_ID;
 const CLIENT_SECRET = process.env.REACT_APP_CLIENT_SECRET;
 
+
+const withCreds = url => {
+  return `${url}client_id=${CLIENT_ID}&client_secret=${CLIENT_SECRET}`
+}
+
 export const GithubState = ({children}) => {
   const initialState = {
     user: {},
@@ -21,7 +26,7 @@ export const GithubState = ({children}) => {
     setLoading();
 
     const response = await axios.get(
-      `https://api.github.com/search/users?q=${value}&client_id=${CLIENT_ID}&client_secret=${CLIENT_SECRET}`
+      withCreds(`https://api.github.com/search/users?q=${value}&`)
     )
 
     dispatch({
@@ -32,19 +37,27 @@ export const GithubState = ({children}) => {
 
   const getUser = async name => {
     setLoading()
+
+    const response = await axios.get(
+      withCreds(`https://api.github.com/users/${name}?`)
+    )
     // ...
     dispatch({
       type: GET_USER,
-      payload: []
+      payload: response.data
     })
   }
 
   const getRepos = async name => {
     setLoading()
+
+    const response = await axios.get(
+      withCreds(`https://api.github.com/users/${name}/repos?per_page=5&`)
+    )
     // ...
     dispatch({
       type: GET_REPOS,
-      payload: []
+      payload: response.data
     })
   }
 
