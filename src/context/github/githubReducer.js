2d88a42e2f19d091e225ce7af90ee28a2e583b88
Salnikov Maksim <salnikov-m@yandex.ru>
2020-09-17 11:15:12 +0400

    Hooks. Lesson 9 is done!

--- src/context/github/githubReducer.js
+++ src/context/github/githubReducer.js
@@ -1,4 +1,4 @@
-import {GET_REPOS, SEARCH_USERS} from "../types";
+import {CLEAR_USERS, GET_REPOS, GET_USER, SEARCH_USERS, SET_LOADING} from "../types";
 
 const handlers = {
   [SEARCH_USERS]: (state, {payload}) => ({...state, users: payload, loading: false}),
